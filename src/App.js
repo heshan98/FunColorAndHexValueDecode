e90f8ae9e4bb20131e2ffef635e8d967af18d044
heshan <heshandinushka98@gmail.com>
2023-01-21 09:06:56 +0530

    Code Challenge

--- src/App.js
+++ src/App.js
@@ -1,24 +1,54 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import colorNames from 'colornames';
 
 function App() {
+  // const handleSubmit = (e) => {
+  //   e.preventDefault();
+  //   // console.log(newItem)
+  // }
+  const [newItem, setNewItem] = useState('')
+  const [hexColor, setHexColor] = useState('')
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+
+    <div className="App" style={{ paddingTop: '30px' }}>
+
+      <div style={{
+        backgroundColor: `${newItem}`,
+        fontSize: '30px', paddingTop: '40px', width: '50%', textAlign: 'center',
+         margin: 'auto', paddingBottom: '25px', border: '3px solid black'
+      }}>
+        {newItem.length ? newItem : 'Type the color name And Get color And Hex Value'}
+        <p>{hexColor}</p>
+      </div>
+
+      <form className='submit' style={{ paddingTop: '20px' }}>
+        <label htmlFor='additem'>Type Color :</label>&nbsp;&nbsp;
+        <input
+          autoFocus
+          id='addItem'
+          type='text'
+          placeholder='ex:red'
+          required
+
+          onChange={(e) => {
+            setNewItem(e.target.value)
+            setHexColor(colorNames(e.target.value))
+
+
+              (console.log(e.target.value))
+          }
+
+          }
+
+        />
+
+
+      </form>
     </div>
+
   );
 }
 
